6980227360d617e63013c6fe985aad2f0b06c4c2
Eric Ziko <eric.ziko@gmail.com>
2024-10-19 05:27:59 -0700

    added spectre-console

---
+++ src/ConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+using FakeItEasy;
+using Spectre.Console.Testing;
+
+namespace ConsoleApp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        // From https://anthonysimmon.com/beautiful-interactive-console-apps-with-system-commandline-and-spectre-console/
+        
+        var projectManager = A.Fake<IProjectManager>();
+        A.CallTo(() => projectManager.ProjectTypes).Returns(["a", "b", "c", "d"]);
+
+        var console = new TestConsole();
+        console.Profile.Capabilities.Interactive = true;
+        console.Input.PushKey(ConsoleKey.DownArrow);
+        console.Input.PushKey(ConsoleKey.DownArrow);
+        console.Input.PushKey(ConsoleKey.Enter);
+
+        var handler = new CreateProjectCommandOptionsHandler(console, projectManager);
+
+        var result = await handler.HandleAsync(new CreateProjectCommandOptions(), CancellationToken.None);
+
+        Assert.That(result, Is.EqualTo(0));
+
+        A.CallTo(() => projectManager.CreateProjectAsync("c", CancellationToken.None))
+            .MustHaveHappenedOnceExactly();
+        Assert.Pass();
+    }
+}
